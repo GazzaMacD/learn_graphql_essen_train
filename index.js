ec3b6bed2a70c5edbcf29d683098ba0643bcb681
Gareth Macdonald <macdonald.gareth@gmail.com>
2021-03-18 10:08:05 +0100

    add prettier and graphql-tools to project

--- index.js
+++ index.js
@@ -1,38 +1,26 @@
-import express from 'express';
-import schema from './schema.js';
+import express from "express";
+import schema from "./schema.js";
 
-import { graphqlHTTP } from 'express-graphql';
-
-
+import { graphqlHTTP } from "express-graphql";
+import resolvers from "./resolvers.js";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Graphql learning is fun!');
+app.get("/", (req, res) => {
+  res.send("Graphql learning is fun!");
 });
 
-/* 
-* this is a resolver which typically would resolve something 
-* from the database but for now it is hard coded
-*/
-const root = { friend: () => {
-    return {
-        'id': 183647783838,
-        'firstName' : 'Gareth', 
-        'lastName' : 'Macdonald',
-        'gender' : 'Male', 
-         'email' : 'macdonald.gareth@gmail.com' 
-    }
-}};
+const root = resolvers;
 
-app.use('/graphql', graphqlHTTP(
-    {
-        schema: schema,
-        rootValue: root,
-        graphiql: true
-    }
-))
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 app.listen(8080, () => {
-    console.log('Running on server port localhost:8080/graphql')
+  console.log("Running on server port localhost:8080/graphql");
 });
